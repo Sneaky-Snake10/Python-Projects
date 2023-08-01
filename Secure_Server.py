055f4893e92de5bf4f1e0c87e093f9cb82561dda
Sneaky-Snake10 <128928184+Sneaky-Snake10@users.noreply.github.com>
2023-08-01 16:38:22 -0700

    Update Secure_Server.py
    Add documentation

--- Secure_Server.py
+++ Secure_Server.py
@@ -1,3 +1,4 @@
+# Try to import the necessary libraries
 try:
     import socket
     # Cryptography Libraries
@@ -6,10 +7,12 @@ try:
     from Crypto.Cipher import PKCS1_OAEP
 
 except ImportError:
+    # Inform the user of the missing libraries
     raise OSError('The neccasary libraries are not found. Make sure that socket, pycryptodome and cryptography are installed.')
 
 # Define SecureSession Class
 class SecureSession:
+    # This class is an interface to the encrypted connection, that acts like the "socket" object, but also performs the encryption.
     def __init__(this, connection, session):
         this.connection = connection
         this.session = session
@@ -19,31 +22,42 @@ class SecureSession:
         if this.closed:
             # Reference to the socket library socket closed error, "An operation was attempted on something that is not a socket."
             raise OSError('An operation was attempted on something that is not a connection.')
+         # If it is not closed, encrypt and send the data. Similar to the socket library, it accepts a bytes-like object.
         this.connection.send(this.session.encrypt(data))
         return len(this.session.encrypt(data))
     def recv(this, bytes):
         if this.closed:
             # Reference to the socket library socket closed error, "An operation was attempted on something that is not a socket."
             raise OSError('An operation was attempted on something that is not a connection.')
+         # If it is not closed, then return the decrypted bytes. Similar to the socket library, it returns a bytes-like object.
         return this.session.decrypt(this.connection.recv(bytes))
     
     def close(this):
+        # Set the closed flag, and close the TCP connection.
         this.closed = True
+        this.connection.close()
         return None
 # Define Session Generator
 def init_session(connection):
+    # Give the client the server's public key.
     connection.send(str(public_key.n).encode())
+    # Decrypt the session key for this connection.
     sessionkey = secret.decrypt(connection.recv(1048576))
+    # Decrypt the client's challenge, and send back the bytes.
     connection.send(secret.decrypt(connection.recv(1024)))
+    # Generate a Fernet object to pass to the SecureSession.
     session = Fernet(sessionkey)
+    # Return a Session object, that performs encryption/decryption with the newly established symmetric session key.
     return SecureSession(connection=connection, session=session)
 # RSA Key Generation
 private_key = RSA.generate(2048)
 public_key = private_key.publickey()
+# Set up the two keys with objects that will perform encryption/decryption with them.
 cipher = PKCS1_OAEP.new(public_key)
 secret = PKCS1_OAEP.new(private_key)
-# Demonstration of library, does not run when imported.
+
 if __name__ == '__main__':
+    # Demonstration of how to use this script. Here, the variable "secure_conn" is the SecureSession object, and provides an interface into the encrypted connection.
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind(('', 7000))
     sock.listen(1)

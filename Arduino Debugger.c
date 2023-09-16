dc6c98b425dd1538f930e0e4c5b42fab69e9360b
Sneaky-Snake10 <128928184+Sneaky-Snake10@users.noreply.github.com>
2023-09-15 17:44:13 -0700

    Update Arduino Debugger.c

--- Arduino Debugger.c
+++ Arduino Debugger.c
@@ -5,45 +5,65 @@ void setup() {
   // Initialize GPIO pins as outputs
   for (int i = 0; i < sizeof(gpio_pins) / sizeof(gpio_pins[0]); i++) {
     pinMode(gpio_pins[i], OUTPUT);
-    digitalWrite(gpio_pins[i], LOW);
+    digitalWrite(gpio_pins[i], LOW); // Set pins to LOW (OFF) initially
   }
   
-  // Initialize Serial communication
+  // Initialize Serial communication at 9600 baud rate
   Serial.begin(9600);
 }
 
 void loop() {
   if (Serial.available() > 0) {
+    // Read the incoming message until a newline character is received
     String message = Serial.readStringUntil('\n');
-    message.trim();
+    message.trim(); // Remove leading/trailing whitespace
 
+    // Check if the message starts with "ON "
     if (message.startsWith("ON ")) {
+      // Extract the pin number from the message and convert it to an integer
       int pin_number = message.substring(3).toInt();
+      
+      // Check if the pin number is valid
       if (isValidPin(pin_number)) {
+        // Turn on the specified GPIO pin
         digitalWrite(pin_number, HIGH);
+        // Send "OK" back to the sender
         Serial.println("OK");
       } else {
+        // Send "Invalid Command" if the pin is not in the list
         Serial.println("Invalid Command");
       }
-    } else if (message.startsWith("OFF ")) {
+    }
+    // Check if the message starts with "OFF "
+    else if (message.startsWith("OFF ")) {
+      // Extract the pin number from the message and convert it to an integer
       int pin_number = message.substring(4).toInt();
+      
+      // Check if the pin number is valid
       if (isValidPin(pin_number)) {
+        // Turn off the specified GPIO pin
         digitalWrite(pin_number, LOW);
+        // Send "OK" back to the sender
         Serial.println("OK");
       } else {
+        // Send "Invalid Command" if the pin is not in the list
         Serial.println("Invalid Command");
       }
-    } else {
+    }
+    // If the message format is not recognized
+    else {
       Serial.println("Invalid Command");
     }
   }
 }
 
+// Function to check if a pin number is valid
 bool isValidPin(int pin) {
   for (int i = 0; i < sizeof(gpio_pins) / sizeof(gpio_pins[0]); i++) {
+    // Compare the pin number with the elements in the gpio_pins array
     if (gpio_pins[i] == pin) {
-      return true;
+      return true; // The pin is valid
     }
   }
-  return false;
+  return false; // The pin is not valid
 }
